3b46548f7a4c1b1203a952effde198abc5aee534
Arron-B <arronmail@yahoo.com>
2023-10-23 10:56:03 +0100

    repo setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import { Card } from "react-bootstrap";
+
+function App() {
+	return (
+		<>
+			<Card>
+				<Card.Title>Hello</Card.Title>
+			</Card>
+		</>
+	);
+}
+
+export default App;
