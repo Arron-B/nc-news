78c4ad8e9b4e76ad7a8f747ef1050be29ea5377c
Arron-B <arronmail@yahoo.com>
2023-10-24 15:39:42 +0100

    voting on article basic functionality implemented

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,24 +1,34 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
-import { fetchArticleById, fetchCommentsByArticleId } from "../api";
+import {
+	fetchArticleById,
+	fetchCommentsByArticleId,
+	voteOnArticle,
+} from "../api";
 import {
 	Card,
 	Container,
 	Spinner,
 	Button,
 	DropdownButton,
+	Overlay,
 } from "react-bootstrap";
 import { capitaliseFirstLetter } from "../utils/utils";
 
 function Article() {
 	const { article_id } = useParams();
 
+	const target = useRef(null);
+
 	const [article, setArticle] = useState(false);
 	const [votes, setVotes] = useState(0);
 	const [comments, setComments] = useState([]);
+	const [voteFail, setVoteFail] = useState(false);
+	const [voteSuccess, setVoteSuccess] = useState(false);
+	const [errMsg, setErrMsg] = useState("Something went wrong!");
+	const [votesDisabled, setVotesDisabled] = useState(false);
 
 	useEffect(() => {
-		console.log("fetching article");
 		fetchArticleById(article_id)
 			.then((res) => {
 				setArticle(res.data.article);
@@ -30,6 +40,29 @@ function Article() {
 			});
 	}, []);
 
+	function handleVote(increment) {
+		setVotes(votes + increment);
+		voteOnArticle(article_id, increment)
+			.then((res) => {
+				setVotesDisabled(true);
+				if (res.status === 200) {
+					setVoteSuccess(true);
+					setTimeout(() => {
+						setVoteSuccess(false);
+					}, 5000);
+				}
+			})
+			.catch((err) => {
+				setErrMsg(err.message);
+				setVoteFail(true);
+				setVotesDisabled(false);
+				setVotes(votes - increment);
+				setTimeout(() => {
+					setVoteFail(false);
+				}, 5000);
+			});
+	}
+
 	if (article) {
 		return (
 			<Card>
@@ -39,24 +72,93 @@ function Article() {
 				<Card.Subtitle>Author: {article.author}</Card.Subtitle>
 				<Card.Img
 					variant="top"
+					alt={`an image for article titled ${article.title}`}
 					src={article.article_img_url}
 				/>
 				<Card.Text>{article.body}</Card.Text>
 				<Card.Text>Votes: {votes}</Card.Text>
 				<Button
+					className="w-50"
 					variant="light"
 					size="sm"
-					onClick={() => console.log("Light")}
+					disabled={votesDisabled}
+					ref={target}
+					onClick={(e) => {
+						e.preventDefault();
+						handleVote(1);
+					}}
 				>
 					Vote Up
 				</Button>
 				<Button
+					className="w-50"
 					variant="light"
 					size="sm"
-					onClick={() => console.log("Light")}
+					disabled={votesDisabled}
+					ref={target}
+					onClick={(e) => {
+						e.preventDefault();
+						handleVote(-1);
+					}}
 				>
 					Vote Down
 				</Button>
+				<Overlay
+					target={target.current}
+					show={voteSuccess}
+					placement="right"
+				>
+					{({
+						placement: _placement,
+						arrowProps: _arrowProps,
+						show: _show,
+						popper: _popper,
+						hasDoneInitialMeasure: _hasDoneInitialMeasure,
+						...props
+					}) => (
+						<div
+							{...props}
+							style={{
+								position: "absolute",
+								backgroundColor: "rgba(28, 184, 28, 0.85)",
+								padding: "2px 10px",
+								color: "white",
+								borderRadius: 3,
+								...props.style,
+							}}
+						>
+							Vote Successful!
+						</div>
+					)}
+				</Overlay>
+				<Overlay
+					target={target.current}
+					show={voteFail}
+					placement="right"
+				>
+					{({
+						placement: _placement,
+						arrowProps: _arrowProps,
+						show: _show,
+						popper: _popper,
+						hasDoneInitialMeasure: _hasDoneInitialMeasure,
+						...props
+					}) => (
+						<div
+							{...props}
+							style={{
+								position: "absolute",
+								backgroundColor: "rgba(184, 28, 28, 0.85)",
+								padding: "2px 10px",
+								color: "white",
+								borderRadius: 3,
+								...props.style,
+							}}
+						>
+							{errMsg}
+						</div>
+					)}
+				</Overlay>
 				<DropdownButton
 					title="Comments"
 					id="comments-dropdown"
