f7d30838df1cba80e96d9fddb65172831210d20b
Arron-B <arronmail@yahoo.com>
2023-10-27 16:15:45 +0100

    Removed loading new comment when comment post fails
    Message shows when failing to delete a comment
    
    An indvidual delete button will be inactive for 3 secs upon pressing
    
    submit comment button is now inactive for 5 secs upon attempting to submit a comment
    
    an error page added for bad paths
    
    Still need to handle errors for bad queries such as non-existant topic

---
+++ src/components/BadPath.jsx
@@ -0,0 +1,19 @@
+import { Image } from "react-bootstrap";
+
+function BadPath() {
+	return (
+		<>
+			<main className="h-100 d-flex flex-column align-items-center justify-content-center">
+				<h1>You shall not path!...</h1>
+				<h1>to this extension.</h1>
+				<Image
+					className="w-75"
+					src="https://cdn.vox-cdn.com/thumbor/b2zu12nLBrVwxbLqFLKV1jUaMvU=/626x0:3657x1587/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/23003314/4k_fellowship_movie_screencaps.com_23524.jpg"
+				></Image>
+				<h2>Sorry there's nothing here</h2>
+			</main>
+		</>
+	);
+}
+
+export default BadPath;
